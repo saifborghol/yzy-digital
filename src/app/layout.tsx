2439e966e90ae8de82619063482dadad906cb93d
saifborghol <saifborghol@gmail.com>
2024-11-11 21:35:24 +0100

    edit layout content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,11 @@ type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-const APP_NAME = 'nextarter-chakra';
+const APP_NAME = 'technical-test-for-yzy-by-saif';
 
 export const metadata: Metadata = {
-  title: { default: APP_NAME, template: '%s | nextarter-chakra' },
-  description: 'Next.js + chakra-ui + TypeScript template',
+  title: { default: APP_NAME, template: '%s | technical-test-for-yzy-by-saif' },
+  description: 'technical-test-for-yzy-by-saif',
   applicationName: APP_NAME,
   appleWebApp: {
     capable: true,
@@ -21,19 +21,6 @@ export const metadata: Metadata = {
   },
   formatDetection: {
     telephone: false,
-  },
-  openGraph: {
-    url: 'https://nextarter-chakra.sznm.dev',
-    title: 'nextarter-chakra',
-    description: 'Next.js + chakra-ui + TypeScript template',
-    images: {
-      url: 'https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250',
-      alt: 'nextarter-chakra.sznm.dev og-image',
-    },
-  },
-  twitter: {
-    creator: '@sozonome',
-    card: 'summary_large_image',
   },
 };
 
